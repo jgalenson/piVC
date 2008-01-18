78cd1f1649dacb5dfcbf70584588b417ac4086ef
Joel Galenson <galenson@cs.stanford.edu>
2008-01-18 20:25:35 +0000

    Wrote it the more efficient way because it's been annoying me.

--- server/verification_conditions.ml
+++ server/verification_conditions.ml
@@ -4,12 +4,6 @@ open Basic_paths ;;
 
 exception InvalidPath of string ;;
 
-(* Returns (last_elem, rest of list) of a list.
-   E.g. remove_last_elem [1, 2, 3, 4] = (4, [1, 2, 3]) *)
-let remove_last_elem l = 
-  let rev_list = List.rev l in
-  (List.hd rev_list, List.rev (List.tl rev_list)) ;;
-
 (* Gets the Ast.Expr out of a path node. *)
 let get_expr_from_path_node n = match n with
   | Annotation (e, _) -> e
@@ -21,11 +15,12 @@ let get_vc path =
   
   let dummy_loc = Ast.get_dummy_location () in
   let start_ann = get_expr_from_path_node (List.hd path) in
-  let (end_node, instructions) = remove_last_elem (List.tl path) in
-  let end_ann = get_expr_from_path_node end_node in
+  let rev_list = List.rev (List.tl path) in
+  let end_ann = get_expr_from_path_node (List.hd rev_list) in
+  let rev_instrs = List.tl rev_list in
 
-  (* Weakest precondition on a list: wp(formula, i1; i2; ...; in) *)
-  let rec wp formula instrs =
+  (* Weakest precondition on a list in reverse order: wp(formula, in; ...; i2; i1) *)
+  let rec wp formula rev_instrs =
 
     (* Weakest precondition on a single instruction: wp(formula, instr) *)
     let single_wp formula instr =
@@ -41,16 +36,15 @@ let get_vc path =
       | _ -> raise (InvalidPath "Annotation in middle of path")
     in
     
-    if List.length instrs = 0 then
+    if List.length rev_instrs = 0 then
       raise (InvalidPath "No instructions in path")
-    else if List.length instrs = 1 then
-      single_wp formula (List.hd instrs)
+    else if List.length rev_instrs = 1 then
+      single_wp formula (List.hd rev_instrs)
     else
-      let (last_instr, rest_of_instrs) = remove_last_elem instrs in
-      wp (single_wp formula last_instr) rest_of_instrs
+      wp (single_wp formula (List.hd rev_instrs)) (List.tl rev_instrs)
   in
   
-  Ast.Implies (dummy_loc, start_ann, wp end_ann instructions) ;;
+  Ast.Implies (dummy_loc, start_ann, wp end_ann rev_instrs) ;;
 
 (* Print a verification condition. *)
 let print_vc vc =
