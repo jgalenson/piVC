b9403362423822f34d7461e8a367693139c4d888
Jason Auerbach <jasonaue@cs.stanford.edu>
2008-04-01 23:55:01 +0000

    Proposed timetable
    I added some deadlines to the TODO file. Feel free to modify as you deem 
    fit. Note that the file doesn't reflect when we will be doing stuff, but 
    rather when stuff will be done by. For example, I have put 
    proof of termination in week 7, but obviously it will take longer than 
    one week to do that.

--- utils/constants.ml
+++ utils/constants.ml
@@ -1,3 +1,4 @@
 let main_server_port = 4242 ;;
 let dp_server_port = 4243 ;;
-let dp_server_address = "128.12.102.200" ;; (* TODO: Yuck. *)
+(*let dp_server_address = "128.12.102.200" ;; (* TODO: Yuck. *)*)
+let dp_server_address = "127.0.1.1" ;; (* TODO: Yuck. *)
