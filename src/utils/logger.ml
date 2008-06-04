ea571e7c7c6341f244bbeef680d800ab41f1331e
Joel Galenson <galenson@cs.stanford.edu>
2008-06-04 23:29:43 +0000

    Some logging work.
    We did assign logging to me, right?
    
    A few questions:
    
    Should we do any kind of automated log rotation, or should we just always append?
    
    What do we want to log?  Obviously exceptions/errors, and I think we wanted to log all main server connections.  Is there anything else?
    
    How should we handle the config for this?  If we do it the normal way, we'd put a log_file key in both config files.  But this would let you log to different files for the different servers.  It's probably better to always use one file and inside differentiate which is logging it (which we currently are on track to do).  So should we add a third conf file for shared things?  This could also apply to other things, such as truncate_output.

--- src/utils/logger.ml
+++ src/utils/logger.ml
@@ -1,2 +1,56 @@
-let log msg = 
-  ignore ()
+open Unix ;;
+
+let log_lock = Mutex.create () ;;
+
+(* Gets a string out of the current time.
+   It is of the form: Jun 4 16:21:42 *)
+let get_time_str () =
+  let time = Unix.localtime (Unix.time ()) in
+  let month_str = match time.tm_mon with
+    | 0 -> "Jan"
+    | 1 -> "Feb"
+    | 2 -> "Mar"
+    | 3 -> "Apr"
+    | 4 -> "May"
+    | 5 -> "Jun"
+    | 6 -> "Jul"
+    | 7 -> "Aug"
+    | 8 -> "Sep"
+    | 9 -> "Oct"
+    | 10 -> "Nov"
+    | 11 -> "Dec"
+    | _ -> assert false (* Month is only 0-11 as given in docs. *)
+  in
+  let soi = string_of_int in
+  (* Pad length 1 numbers to length 2. *)
+  let soi_pad x =
+    let str = string_of_int x in
+    if String.length str = 1 then
+      "0" ^ str
+    else
+      str
+  in
+  month_str ^ " " ^ (string_of_int time.tm_mday) ^ " " ^ (soi_pad time.tm_hour) ^ ":" ^ (soi time.tm_min) ^ ":" ^ (soi_pad time.tm_sec) ;;
+
+(* Gets a string of the currently-running server. *)
+let get_server_type_str () =
+    let server_type = Config.get_server_type () in
+    match server_type with
+      | Some (Config.MainServer) -> "main server"
+      | Some (Config.DPServer) -> "dp server"
+      | _ -> "unknown server" ;;
+
+let log msg =
+  print_endline "Hi";
+  (* TODO: Check Config to see if logging is enabled. *)
+  (* TODO: Get logging filename from Config. *)
+  let filename = "pivc.log" in
+  Mutex.lock log_lock;
+  let log_file = Unix.openfile filename [Unix.O_CREAT; Unix.O_WRONLY; Unix.O_APPEND] 0o640 in
+  let write_to_log fd str =
+    let full_str = get_time_str () ^ " " ^ get_server_type_str () ^ ": " ^ str ^ "\n" in
+    ignore (Unix.write fd full_str 0 (String.length full_str))
+  in
+  write_to_log log_file msg;
+  Unix.close log_file;
+  Mutex.unlock log_lock
